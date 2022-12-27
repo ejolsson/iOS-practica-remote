84d909bd929b17e7e4434e1ba0a5bcc96e52124d
Eric Olsson <ejolsson@gmail.com>
2022-12-27 20:01:48 +0100

    TabBar now entry point to the app

--- ios-practica/ios-practica/App/SceneDelegate.swift
+++ ios-practica/ios-practica/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = HeroListViewController()
+        window?.rootViewController = TabBarController() // whichever VC is listed, will be the entry VC to the app
         window?.makeKeyAndVisible()
     }
 
