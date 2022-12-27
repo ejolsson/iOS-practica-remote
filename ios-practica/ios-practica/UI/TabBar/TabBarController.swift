e9b643553ce5f2323b0531bb9ed4e310ace446ed
Eric Olsson <ejolsson@gmail.com>
2022-12-27 19:54:25 +0100

    Add base to TabBarController, no output yet

--- ios-practica/ios-practica/UI/TabBar/TabBarController.swift
+++ ios-practica/ios-practica/UI/TabBar/TabBarController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTabs()
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    private func setupTabs() {
+        let navigationController1 = UINavigationController(rootViewController: HeroListViewController())
+        let tabImage1 = UIImage(systemName: "text.justify")!
+        navigationController1.tabBarItem = UITabBarItem(title: "Hero List", image: tabImage1, tag: 0)
+        
+        let navigationController2 = UINavigationController(rootViewController: SettingsViewController())
+        let tabImage2 = UIImage(systemName: "square.grid.3x3.topleft.filled")!
+        navigationController2.tabBarItem = UITabBarItem(title: "Settings", image: tabImage2, tag: 1)
+        
+        viewControllers = [navigationController1, navigationController2]
     }
-    */
 
 }
