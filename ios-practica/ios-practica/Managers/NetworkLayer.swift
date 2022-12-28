7bd6bf41b0a152143935f12e02edfe3dc5088da1
Eric Olsson <ejolsson@gmail.com>
2022-12-28 11:22:58 +0100

    Added NetworkLayer, not connected yet. Build OK, same output as before.

---
+++ ios-practica/ios-practica/Managers/NetworkLayer.swift
@@ -0,0 +1,103 @@
+//
+//  NetworkLayer.swift
+//  ios-practica
+//
+//  Created by Eric Olsson on 12/28/22.
+//  Used for API connection
+
+import Foundation
+
+enum NetworkError: Error {
+    case malformedURL
+    case noData
+    case statusCode(code: Int?)
+    case decodingFailed
+    case unknown
+}
+
+final class NetworkLayer {
+    
+    static let shared = NetworkLayer()
+    
+    func login(email: String, password: String, completion: @escaping (String?, Error?) -> Void) {
+        guard let url = URL(string: "https://dragonball.keepcoding.education/api/auth/login") else {
+            completion(nil, NetworkError.malformedURL)
+            return
+        }
+     
+        let loginString = "\(email):\(password)"
+        let loginDate: Data = loginString.data(using: .utf8)!
+        let base64 = loginDate.base64EncodedData()
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Basic \(base64)", forHTTPHeaderField: "Authorization")
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, NetworkError.noData)
+                return
+            }
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                completion(nil, NetworkError.statusCode(code: statusCode))
+                return
+            }
+            
+            guard let token = String(data: data, encoding: .utf8) else {
+                completion(nil, NetworkError.decodingFailed)
+                return
+            }
+            
+            completion(token, nil)
+        }
+        
+        task.resume()
+    }
+    
+    func fetchHeros(token: String?, completion: @escaping ([Hero]?, Error?) -> Void) {
+        
+        // local function definitions
+        guard let url = URL(string: "https://dragonball.keepcoding.education/api/heros/all") else {
+            completion(nil, NetworkError.malformedURL)
+            return
+        }
+        
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = [URLQueryItem(name: "name", value: "")]
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Bearer \(token ?? "")", forHTTPHeaderField: "Authorization")
+        urlRequest.httpBody = urlComponents.query?.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, NetworkError.noData)
+                return
+            }
+            
+            guard let heros = try? JSONDecoder().decode([Hero].self, from: data) else {
+                completion(nil, NetworkError.decodingFailed)
+                return
+            }
+            
+            completion(heros, nil)
+        }
+        
+        task.resume()
+    }
+    
+}
